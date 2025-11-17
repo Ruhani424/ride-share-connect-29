0950de022c7e93de7946ae966e6772d3e65b45ff
Ruhani Sachdeva <ruhani0701.becse24@chitkara.edu.in>
2025-11-17 09:23:01 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Car, Facebook, Twitter, Instagram, Linkedin } from "lucide-react";
+import { Car, Facebook, Twitter, Instagram, Linkedin } from "lucide-react"
 
 const Footer = () => {
   return (
